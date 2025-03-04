262627e5ca1015f35694b9d7bed5083ad66cf437
Divarahma22 <divanurrahmawati202@gmail.com>
2025-03-04 16:33:26 +0700

    update7

--- dashboard.py
+++ dashboard.py
@@ -3,8 +3,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import matplotlib.pyplot as plt
-print(plt)
+
 # Atur gaya Seaborn
 sns.set_theme(style="whitegrid", context="talk")
 
